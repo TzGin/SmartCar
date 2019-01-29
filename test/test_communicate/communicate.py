35cae8b329794064ec5f41a2bc9047de6713d69b
ZJing <fzjtxz@gmail.com>
2019-01-29 15:25:21 +0800

    cleanup code

--- test/test_communicate/communicate.py
+++ test/test_communicate/communicate.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 '''
-@File    :   serial.py
+@File    :   serial_to_ev3.py
 @Time    :   2019/01/19 11:24:46
 @Author  :   Zijing Feng
 '''
 
-import serial.Serial
 import sys
 sys.path.append(r"/home/robot/SmartCar-master/")
+import serial
 from lib import debug_print
 
-
-s = Serial('/dev/ttyACM0', 9600)
+s = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
     data = s.readline()
-    print(data)
-    debug_print(data, file=sys.stderr)
+    debug_print(data)
